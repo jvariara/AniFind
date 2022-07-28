707eaaeddd925c56df7a56c227586ea783863e27
jvariara <jvariara@gmail.com>
2022-07-27 21:49:44 -0400

    Fixed Routing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function storeSearchElem(event) {
   const animeSearch = event.target.value;
   localStorage.setItem("anime", animeSearch);
-  window.location.href = `${window.location.origin}/html/findyouranime.html`;
+  window.location.href = `${window.location.origin}/anifind/html/findyouranime.html`;
 }
